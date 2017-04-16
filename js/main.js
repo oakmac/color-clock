98b780771fb72c1a872683ed63ed1ed4cc961d0a
jennypenfield <jipenfield11@gmail.com>
2017-04-16 11:48:52 -0500

    updated javascript with working gradients -- Line 40

--- js/main.js
+++ js/main.js
@@ -13,38 +13,40 @@ function init () {
   }, 1000)
 
   function makeTwoDigits (time) {
-    if (time < 10) {
+    if (time.length < 2) {
       return '0' + time
     } else {
       return time
     }
   }
-  // getHexGradient()
-  // function getHexGradient () {
-  //   var date = new Date()
-  //   var hours = makeTwoDigits(date.getHours()).toString(16)
-  //   var minutes = makeTwoDigits(date.getMinutes()).toString(16)
-  //   var seconds = makeTwoDigits(date.getSeconds()).toString(16)
-  //   isHex = true
-  //   return '#' + hours + ':' + minutes + ':' + seconds
-  // }
 
-  function setHexGradient (hr, min, sec) {
-    var hourHex = hr.toString(16)
-    var minHex = min.toString(16)
-    var secHex = sec.toString(16)
+  getHexGradient()
+  function getHexGradient () {
+    var date = new Date()
+    var hours = makeTwoDigits(date.getHours()).toString(16)
+    var minutes = makeTwoDigits(date.getMinutes()).toString(16)
+    var seconds = makeTwoDigits(date.getSeconds()).toString(16)
+    isHex = true
+    return '#' + hours + ':' + minutes + ':' + seconds
+  }
+
+  function setHexGradient (hours, minutes, seconds) {
+    var hourHex = makeTwoDigits(hours.toString(16))
+    var minHex = makeTwoDigits(minutes.toString(16))
+    var secHex = makeTwoDigits(seconds.toString(16))
     var hexGradient = function () {
       return '#' + hourHex + minHex + secHex
     }
-    // var hexGradient = 'red'
-    $('.gradient').css('background', 'radial-gradient(circle, #fff, ' + hexGradient())
+    $('.gradient').css('background', 'radial-gradient(circle, #fff, ' + hexGradient() + ')')
+    console.log(hexGradient())
   }
 
-  // $('p#time').hover(function () {
-  //   if (isHex) {
-  //   } else {
-  //     $('p#time').html(hexColor)
-  //   }
-  // })
+  $('p#time').hover(function () {
+    if (isHex) {
+      $('p#time').html(getTime())
+    } else {
+      $('p#time').html(getHexGradient)
+    }
+  })
 }
 $(document).ready(init)
